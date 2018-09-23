855f4c34131c7764fcec011fdab7db2659e2f8c5
c10t <c10t@fa.ke>
2018-09-24 01:35:31 +0900

    using config

--- projects/issues/config/config.exs
+++ projects/issues/config/config.exs
@@ -28,3 +28,7 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env()}.exs"
+
+import_config "#{Mix.env}.exs"
+
+config :issues, github_url: "https://api.github.com"
